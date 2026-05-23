9122df6286c6d20194ecdf97ea59e18803ead2c8
Numeez <numeezbaloch17@gmail.com>
2026-05-23 14:41:48 +0530

    Implement AST for let statements

---
+++ parser/parser.go
@@ -0,0 +1,80 @@
+package parser
+
+import (
+	"github.com/Numeez/interpreter/ast"
+	"github.com/Numeez/interpreter/lexer"
+	"github.com/Numeez/interpreter/token"
+)
+
+type Parser struct {
+	l         *lexer.Lexer
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+	p.nextToken()
+	p.nextToken()
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatements()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+	return program
+}
+
+func (p *Parser) parseStatements() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+	if !p.expectPeekToken(token.IDENT) {
+		return nil
+	}
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+	if !p.expectPeekToken(token.ASSIGN) {
+		return nil
+	}
+
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+	return stmt
+
+}
+func (p *Parser) expectPeekToken(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		return false
+	}
+
+}
+
+func (p *Parser) peekTokenIs(tok token.TokenType) bool {
+	return p.peekToken.Type == tok
+}
+func (p *Parser) curTokenIs(tok token.TokenType) bool {
+	return p.curToken.Type == tok
+}
---
+++ parser/parser_test.go
@@ -0,0 +1,64 @@
+package parser
+
+import (
+	"testing"
+
+	"github.com/Numeez/interpreter/ast"
+	"github.com/Numeez/interpreter/lexer"
+)
+
+func TestLetStatements(t *testing.T) {
+	input := `
+	let x = 5;
+	let y = 10;
+	let foobar = 6969;
+	
+	`
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	if program == nil {
+		t.Fatal("program is nil")
+	}
+	if len(program.Statements) != 3 {
+		t.Fatal("program statements are not equal to 3", len(program.Statements))
+	}
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i,tt:= range tests{
+		stmt := program.Statements[i]
+		if !testLetStatement(t,stmt,tt.expectedIdentifier){
+			return
+		}
+	}
+
+}
+
+
+func testLetStatement(t *testing.T,stmt ast.Statement,expected string)bool{
+		if stmt.TokenLiteral()!="let"{
+			t.Errorf("s.TokenLiteral not 'let'. got=%q", stmt.TokenLiteral())
+			return false
+		}
+
+		letStmt,ok:=stmt.(*ast.LetStatement)
+		if !ok{
+			t.Errorf("s not *ast.LetStatement. got=%T", stmt)
+			return false
+		}
+		if letStmt.Name.Value!=expected{
+			t.Errorf("letStmt.Name.Value not '%s'. got=%s", expected, letStmt.Name.Value)
+			return false
+		}
+		if letStmt.Name.TokenLiteral()!=expected{
+			t.Errorf("letStmt.Name.TokenLiteral() not equal to %s got %s",expected,letStmt.Name.TokenLiteral())
+			return false
+		}
+		return  true
+}
